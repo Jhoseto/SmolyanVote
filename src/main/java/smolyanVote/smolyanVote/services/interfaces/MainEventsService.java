8b737347ee685c5e2ff2af6aa4cf3541cd95ad36
Jhoseto <serezliev@gmail.com>
2025-06-18 21:33:25 +0300

    Filter And Search integration v2 Top Optimization/ work Perfect !

--- src/main/java/smolyanVote/smolyanVote/services/interfaces/MainEventsService.java
+++ src/main/java/smolyanVote/smolyanVote/services/interfaces/MainEventsService.java
@@ -3,6 +3,8 @@ package smolyanVote.smolyanVote.services.interfaces;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import smolyanVote.smolyanVote.models.enums.EventStatus;
+import smolyanVote.smolyanVote.models.enums.EventType;
 import smolyanVote.smolyanVote.viewsAndDTO.EventSimpleViewDTO;
 
 import java.util.List;
@@ -11,8 +13,7 @@ public interface MainEventsService {
 
 
     @Transactional(readOnly = true)
-    Page<EventSimpleViewDTO> findAllEvents(String search, String location, String type, String status, Pageable pageable);
-
+    Page<EventSimpleViewDTO> findAllEvents(String search, String location, EventType type, EventStatus status, Pageable pageable);
 
     @Transactional(readOnly = true)
     List<EventSimpleViewDTO> getAllUserEvents(String email);
